bd1ede5b38b1e0b500b11411a889245dc6f26d11
yrashmitha <yrenterprices@gmail.com>
2021-11-04 14:22:43 +0530

    reducer seperated

---
+++ src/store/contactReducer.tsx
@@ -0,0 +1,51 @@
+import { ADD_NEW, DECREMENT, DELETE, INCREMENT, UPDATE } from "./actions";
+import { initialState } from "./initialState";
+
+export const contactReducer = (state: any = initialState, action: any) => {
+  switch (action.type) {
+    case INCREMENT:
+      return {
+        ...state,
+        counter: state.counter + 1,
+      };
+    case DECREMENT:
+      return {
+        ...state,
+        counter: state.counter - 1,
+      };
+
+    case ADD_NEW: {
+      return {
+        ...state,
+        contacts: [...state.contacts, action.user],
+      };
+    }
+
+    case DELETE: {
+      let newArr = [...state.contacts];
+      let index = newArr.findIndex((c) => c.id === action.user.id);
+      newArr.splice(index, 1);
+      return {
+        ...state,
+        contacts: newArr,
+      };
+    }
+    case UPDATE: {
+      let newArr = [...state.contacts];
+      let index = newArr.findIndex((c) => c.id === action.user.id);
+      newArr[index] = {
+        id: action.user.id,
+        name: action.user.name,
+        address: action.user.address,
+        number: action.user.number,
+      };
+      return {
+        ...state,
+        contacts: newArr,
+      };
+    }
+
+    default:
+      return state;
+  }
+};
--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,53 +1,58 @@
 import { createStore } from "redux";
 import { ADD_NEW, DECREMENT, DELETE, INCREMENT, UPDATE } from "./actions";
+import { contactReducer } from "./contactReducer";
 import { initialState } from "./initialState";
 
-const store = createStore((state: any = initialState, action: any) => {
-  switch (action.type) {
-    case INCREMENT:
-      return {
-        ...state,
-        counter: state.counter + 1,
-      };
-    case DECREMENT:
-      return {
-        ...state,
-        counter: state.counter - 1,
-      };
 
-    case ADD_NEW: {
-      return {
-        ...state,
-        contacts: [...state.contacts, action.user],
-      };
-    }
+// const store = createStore((state: any = initialState, action: any) => {
+//   switch (action.type) {
+//     case INCREMENT:
+//       return {
+//         ...state,
+//         counter: state.counter + 1,
+//       };
+//     case DECREMENT:
+//       return {
+//         ...state,
+//         counter: state.counter - 1,
+//       };
 
-    case DELETE: {
-      let newArr = [...state.contacts];
-      let index = newArr.findIndex((c) => c.id === action.user.id);
-      newArr.splice(index, 1);
-      return {
-        ...state,
-        contacts: newArr,
-      };
-    }
-    case UPDATE: {
-      let newArr = [...state.contacts];
-      let index = newArr.findIndex((c) => c.id === action.user.id);
-      newArr[index] = {
-        id: action.user.id,
-        name: action.user.name,
-        address: action.user.address,
-        number: action.user.number,
-      };
-      return {
-        ...state,
-        contacts: newArr,
-      };
-    }
+//     case ADD_NEW: {
+//       return {
+//         ...state,
+//         contacts: [...state.contacts, action.user],
+//       };
+//     }
 
-    default:
-      return state;
-  }
-});
+//     case DELETE: {
+//       let newArr = [...state.contacts];
+//       let index = newArr.findIndex((c) => c.id === action.user.id);
+//       newArr.splice(index, 1);
+//       return {
+//         ...state,
+//         contacts: newArr,
+//       };
+//     }
+//     case UPDATE: {
+//       let newArr = [...state.contacts];
+//       let index = newArr.findIndex((c) => c.id === action.user.id);
+//       newArr[index] = {
+//         id: action.user.id,
+//         name: action.user.name,
+//         address: action.user.address,
+//         number: action.user.number,
+//       };
+//       return {
+//         ...state,
+//         contacts: newArr,
+//       };
+//     }
+
+//     default:
+//       return state;
+//   }
+// });
+
+const store = createStore(contactReducer);
+
 export default store;
